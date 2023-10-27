86310654e61a32414d47f41702562c947bc41b75
MuhammedUmair <misterumair@gmail.com>
2023-10-27 15:03:57 +0400

    signup api working

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SellerService } from '../services/seller.service';
 
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.scss']
 })
-export class SellerAuthComponent {
-  constructor(){}
+export class SellerAuthComponent implements OnInit {
+  constructor(private seller: SellerService){}
 
   ngOnInit():void {}
 
   signUp(data:object):void {
     console.warn(data)
+    this.seller.userSignUp(data).subscribe((result)=>{
+      console.warn(result)
+    });
   }
 }
